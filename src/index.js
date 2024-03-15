fe8dd5568d6c4614873f6a20a668b8fd1798a95e
leGatho <163160514+leGatho@users.noreply.github.com>
2024-03-15 14:24:58 +0100

    Delete src directory

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOM  from "react-dom";
-import TestComponent from "./Paris"
-import "./index.css";
-import Test from "./Paris";
-
-ReactDOM.render(
-    <TestComponent/>, document.getElementById("root")
-);
