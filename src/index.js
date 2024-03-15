9cbc025818cb3a972891a92f6fe7b40b00d4d87a
leGatho <thomasgabin167@gmail.com>
2024-03-15 14:38:09 +0100

    A retenir

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM  from "react-dom";
+import TestComponent from "./Paris"
+import "./index.css";
+import Test from "./Paris";
+
+ReactDOM.render(
+    <TestComponent/>, document.getElementById("root")
+);
